b71042ec80827dcac4dc1c39682ff72453f032f4
Julián Calderone <juliancalderone@Julians-MacBook-Pro.local>
2022-05-10 22:18:29 -0300

    ADD: CardProduc & ItemCount

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,16 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import CardProduct from "../CardProduct/CardProduct";
 import './ItemListContainer.scss';
 
 export default function ItemListContainer({greeting}) {
   return (
     <Container className="list-container">
-      <h5 className="mb-0">{greeting}</h5>
+      {/* <h5 className="mb-0">{greeting}</h5> */}
+      <Row>
+        <Col lg="3" sm="12">
+          <CardProduct />
+        </Col>
+      </Row>
     </Container>
   )
 }
